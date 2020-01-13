f6bff319a0003254d3b23eed82cf37766ba68bc7
Carlo Conserva <carlo.conserva@gmail.com>
2020-01-13 10:13:04 +0000

    Fixed some unit tests

--- test/boundary/data/fixer_rates_repo_test.dart
+++ test/boundary/data/fixer_rates_repo_test.dart
@@ -1,10 +1,10 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:currencies/boundary/data/fixer_api.dart';
 import 'package:currencies/boundary/data/fixer_rates_repo.dart';
 import 'package:currencies/domain/entities.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class MockFixerResponse extends Mock implements FixerResponse {}
 class MockFixerApi extends Mock implements FixerApi {}
 class MockCurrency implements Currency {
   MockCurrency(this.name);
@@ -14,17 +14,24 @@ class MockCurrency implements Currency {
 void main() {
 
   FixerApi fixerApi;
-  FixerResponse fixerResponse;
+  FixerResponseBuilder fixerResponseBuilder;
   FixerConversionRateRepository repo;
 
   setUp(() {
     fixerApi = MockFixerApi();
-    fixerResponse = MockFixerResponse();
+    fixerResponseBuilder = FixerResponseBuilder()
+      ..success = true
+      ..historical = false
+      ..date = ""
+      ..timestamp = 0
+      ..base = ""
+      ..rates = BuiltMap<String, double>.of({}).toBuilder();
     repo = FixerConversionRateRepository(fixerApi);
   });
 
   test("Latest rates: an exception is thrown if the response contains a different base", () async {
-    when(fixerResponse.base).thenReturn("Invalid");
+    fixerResponseBuilder.base = "Invalid";
+    FixerResponse fixerResponse = fixerResponseBuilder.build();
     Currency base = MockCurrency("BASE");
     List<Currency> targets = [MockCurrency("TGT1"), MockCurrency("TGT2")];
     when(fixerApi.getLatestConversionRates("BASE", ["TGT1", "TGT2"])).thenAnswer((_) => Future.value(fixerResponse));
